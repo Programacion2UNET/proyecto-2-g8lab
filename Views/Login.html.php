6678ab8635a151eeaa1cee16149db0f45d2f3c52
Mario <mariojosuexz@gmail.com>
2018-02-20 21:25:33 -0400

    that... .md

---
+++ Views/Login.html.php
@@ -0,0 +1,7 @@
+<a href="/">Back</a>
+<form method="POST" action='login.php' className='login'>
+  <h3>Login</h3>
+  <input type="text" value='' placeholder='name' title='your name'/>
+  <input type="password" value='' placeholder='password' title='your secret'/>
+  <input type="submit" value="send">
+</form>
--- index.php
+++ index.php
@@ -1,21 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- here localtion view controller helper -->
+  <script type="application/javascript" src="/assets/scripts/location.js"></script> 
+  <title>Proyecto 2 - Programacion 2 - UNET</title>
+  <link rel="stylesheet" href="/assets/css/main.css">
+</head>
 <?php
  require_once('./config.php');
  require('./Controllers/View.php');
  $url = $_GET['url'] ?? '/';
 
- # fast req controller
+ echo $url;
+ # fast req controller,ugly way
  switch ($url) {
    case '/':
     $url = './index.html.php';
+    View::render($url, $vars = []);
     break;
   case '/index.php':
     $url = './index.html.php';
+    View::render($url, $vars = []);
+    break;
+  case 'login.php': 
+    $url = './Views/Login.html.php';
+    View::render($url, $vars = []);
     break;
    default: 
     $url = './Views/404.html.php';
     $vars = ['name' => 'demo', 'code' => 404];
+    View::render($url, $vars);
     break;
  };
-
- View::render($url, $vars = []);
 ?>
+</html>
